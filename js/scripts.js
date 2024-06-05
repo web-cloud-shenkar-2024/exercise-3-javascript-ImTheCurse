9b31a9b2218515f37f9803754f4b4a2ffb2c4848
ImTheCurse <33231841+ImTheCurse@users.noreply.github.com>
2024-06-05 17:23:02 +0300

    Feat + Refactor: Added random square coloring, refactored initialaztion code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 window.onload = () => {
+	initRectangles();
+
+}
+
+function initRectangles() {
 	const fullName = ['r', 'a', 'n', 'i', 'g', 'i', 'r', 'o'];
 	fullName.forEach((char) => {
 		const square = document.createElement("div");
 		square.className = "square";
-		console.log(char);
 		square.innerHTML += char;
+		square.style.backgroundColor = chooseRectangleColor();
 		const container = document.getElementById("square-container");
 		container.appendChild(square);
-	}
-	);
-}
-
-function initRectangles() {
+	});
 }
 
 function initSongs() {
 }
 
 function chooseRectangleColor() {
+	const colors = ['green', 'blue', 'red'];
+
+	const random = (min, max) => {
+		const minCeiled = Math.ceil(min);
+		const maxFloored = Math.floor(max);
+		return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+	}
+	return colors[random(0, 2)];
+
 }
 
 function addRectangle() {
